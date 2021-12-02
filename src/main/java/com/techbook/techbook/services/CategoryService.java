3d088704c8abab0fce691fd6dd0af5ab4a68d3bf
marcosspano <e.marcosspano@gmail.com>
2021-12-02 09:20:34 -0300

    Seeds

--- src/main/java/com/techbook/techbook/services/CategoryService.java
+++ src/main/java/com/techbook/techbook/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.techbook.techbook.services;
 
-import com.techbook.techbook.entities.Book;
 import com.techbook.techbook.entities.Category;
 import com.techbook.techbook.repositories.IBookRepository;
 import com.techbook.techbook.repositories.ICategoryRepository;
@@ -20,22 +19,6 @@ public class CategoryService {
     public List<Category> listCategories() {
         return categoryRepository.findAll();
     }
-
-//    public void loadData() {
-//
-//        categoryRepository.save(new Category("INFORMATICA"));
-//        categoryRepository.save(new Category("DIREITO"));
-//        categoryRepository.save(new Category("FICCAO"));
-//        categoryRepository.save(new Category("INFANTIL"));
-//
-//        bookRepository.save(new Book("Tech Mundo", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(2)));
-//        bookRepository.save(new Book("Mil maneiras de fazer justiça", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(1)));
-//        bookRepository.save(new Book("Tech Mundo", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(3)));
-//        bookRepository.save(new Book("Mil maneiras de fazer justiça", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(3)));
-//        bookRepository.save(new Book("Tech Mundo", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(4)));
-//        bookRepository.save(new Book("Mil maneiras de fazer justiça", 105.99, "Livro de Tecnologia", "URL", categoryRepository.getById(1)));
-//
-//    }
 
 
 }
