5c0b7dfe483279e315b4c1c3b41898f73d9673ec
marcosspano <e.marcosspano@gmail.com>
2021-12-03 12:45:44 -0300

    Add mail em Profile

--- src/main/java/com/techbook/techbook/entities/Profile.java
+++ src/main/java/com/techbook/techbook/entities/Profile.java
@@ -20,16 +20,19 @@ public class Profile implements Serializable {
     private String urlGit;
     private String urlLinkedin;
     private String urlPhoto;
+    private String mail;
 
     @Lob
     @Column
     private String description;
 
-    public Profile(String fullName, String urlGit, String urlLinkedin, String description) {
+    public Profile(Integer id, String fullName, String urlGit, String urlLinkedin, String urlPhoto, String mail, String description) {
+        this.id = id;
         this.fullName = fullName;
         this.urlGit = urlGit;
         this.urlLinkedin = urlLinkedin;
+        this.urlPhoto = urlPhoto;
+        this.mail = mail;
         this.description = description;
     }
-
 }
