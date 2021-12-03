fd185b932e0b176f29ccbb83e32a6a0e61129dfa
marcosspano <e.marcosspano@gmail.com>
2021-12-03 12:43:22 -0300

    Tabela Profile

--- src/main/java/com/techbook/techbook/entities/Profile.java
+++ src/main/java/com/techbook/techbook/entities/Profile.java
@@ -1,4 +1,35 @@
 package com.techbook.techbook.entities;
 
-public class Profile {
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class Profile implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String fullName;
+    private String urlGit;
+    private String urlLinkedin;
+    private String urlPhoto;
+
+    @Lob
+    @Column
+    private String description;
+
+    public Profile(String fullName, String urlGit, String urlLinkedin, String description) {
+        this.fullName = fullName;
+        this.urlGit = urlGit;
+        this.urlLinkedin = urlLinkedin;
+        this.description = description;
+    }
+
 }
