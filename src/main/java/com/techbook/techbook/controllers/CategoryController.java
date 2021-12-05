012a1aace7835596fe522573bda80e96a72c7ddc
marcosspano <e.marcosspano@gmail.com>
2021-12-05 15:07:09 -0300

    Annotation @CrossOrigin em ProfileController, BookController e CategoryController

--- src/main/java/com/techbook/techbook/controllers/CategoryController.java
+++ src/main/java/com/techbook/techbook/controllers/CategoryController.java
@@ -7,12 +7,14 @@ import com.techbook.techbook.repositories.ICategoryRepository;
 import com.techbook.techbook.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "")
 @RestController
 @RequestMapping("/products/categories")
 public class CategoryController {
