6239751548c6ad1513adb92b1196dd44586ee81a
Giovani Silva <85377008+giovanispaula@users.noreply.github.com>
2021-12-10 22:46:45 -0300

    Foto Marcos

--- src/main/java/com/techbook/techbook/config/Config.java
+++ src/main/java/com/techbook/techbook/config/Config.java
@@ -25,7 +25,7 @@ public class Config implements CommandLineRunner {
     public void run(String...args) throws Exception {
 
         profileRepository.save(new Profile("Giovani Silva", "https://github.com/giovanispaula", "https://www.linkedin.com/in/giovanispaula/", "https://avatars.githubusercontent.com/u/85377008?v=4", "giovannispaula@gmail.com", ""));
-        profileRepository.save(new Profile("Marcos Spano", "https://github.com/marcosspano", "https://www.linkedin.com/in/marcosspano/", "", "e.marcosspano@gmail.com", ""));
+        profileRepository.save(new Profile("Marcos Spano", "https://github.com/marcosspano", "https://www.linkedin.com/in/marcosspano/", "https://avatars.githubusercontent.com/u/65152975?v=4", "e.marcosspano@gmail.com", ""));
         profileRepository.save(new Profile("Nathalia Vieira", "https://github.com/nathsilvavieira", "https://www.linkedin.com/in/nathalia-vieira93/", "https://avatars.githubusercontent.com/u/83051644?v=4", "naahviera@gmail.com", ""));
         profileRepository.save(new Profile("Túlio Xavier", "https://github.com/tuliopxavier", "", "https://avatars.githubusercontent.com/u/68478459?v=4", "tuliopxavier@gmail.com", ""));
 
